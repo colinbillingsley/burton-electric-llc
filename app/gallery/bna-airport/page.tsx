0e8e5f2c0525259cc098a258505e90eaa3ae31f7
Colin Billingsley <129898391+colinbillingsley@users.noreply.github.com>
2025-07-29 16:25:36 -0500

    switched unfinished castle video with horizontal video.

--- app/gallery/bna-airport/page.tsx
+++ app/gallery/bna-airport/page.tsx
@@ -51,20 +51,32 @@ const page = () => {
 
 				{mainProjects[1].videos && mainProjects[1].videos.length > 0 && (
 					<P className="text-muted-foreground my-5">
-						Here are some videos of the BNA Airport projects:
+						Here are some videos of our BNA Airport projects:
 					</P>
 				)}
 
 				{mainProjects[1].videos && (
 					<ul className="grid grid-cols-1 sm:grid-cols-2 gap-4">
 						<li key={"unfinished-castle"}>
-							<video className="w-full rounded-lg h-full" controls>
+							<video
+								className="w-full rounded-lg"
+								width="320"
+								height="240"
+								controls
+								muted
+							>
 								<source src={mainProjects[1].videos[1]} type="video/mp4" />
 								Your browser does not support the video tag.
 							</video>
 						</li>
 						<li key={"finished-castle"}>
-							<video className="w-full rounded-lg" controls>
+							<video
+								className="w-full rounded-lg"
+								width="320"
+								height="240"
+								controls
+								muted
+							>
 								<source src={mainProjects[1].videos[0]} type="video/mp4" />
 								Your browser does not support the video tag.
 							</video>
